0ef4b0aa257533309e6ba718f2c3ac00cb5460c7
azizbaba237 <abdoulazizbaba227@gmail.com>
2025-10-09 17:31:20 +0100

    Ajout de l'historique sur le projet gestion du compte bancaire

--- banque/banque.py
+++ banque/banque.py
@@ -1,5 +1,6 @@
 import json 
 import os 
+import datetime
 """Gestion de banque simple avec des comptes utilisateurs et des transactions basiques.
 """
 
@@ -8,8 +9,8 @@ class CompteBancaire :
     def __init__(self, titulaire, numero_compte):
         self.__titulaire = titulaire
         self.__numero_compte = numero_compte
-        
         self.__solde = 0.0  # Solde initial du compte
+        self.historique = [] # Historique des transactions
         
         
     def __str__(self):
@@ -17,9 +18,10 @@ class CompteBancaire :
     
     
     # Déposer de l'argent sur le compte 
-    def deposer(self, montant) :
+    def deposer(self, montant, ) :
         if montant > 0 :
             self.__solde += montant 
+            self.__enregistrer_transaction("Depot", montant)
             print(f"Dépot de {montant} Fcfa effectuée avec succès.")
         else :
             print("Le montant du dépot ne doit pas etre négatif.")
@@ -28,7 +30,8 @@ class CompteBancaire :
     def retirer(self, montant) :
         if montant > 0 :
             if montant <= self.__solde :
-                self.__solde -= montant 
+                self.__solde -= montant
+                self.__enregistrer_transaction("Retrait", montant) 
                 print(f"Retrait de {montant} Fcfa effectuée avec succès.")
             else : 
                 print("Fonds insuffisants pour effetuer un retrait")
@@ -46,6 +49,35 @@ class CompteBancaire :
             "\n numero_compte": self.__numero_compte,
             "\n solde" : self.__solde
         })
+    
+    # Enregistrer une transaction dans l'historique
+    def __enregistrer_transaction(self, type_transaction, montant) :
+        transaction =  {
+            "type" : type_transaction,
+            "montant" : montant,
+            "date" : datetime.datetime.now().isoformat(),
+            "solde_apres_transaction" : self.__solde
+        }
+        
+        # Ajouter la transaction à l'historique
+        self.historique.append(transaction)
+        
+    # Afficher l'historique des transactions
+    def afficher_historique(self) :
+        
+        # Vérifier si l'historique est vide
+        if not self.historique :
+            print("Aucune transaction n'a été effectée.")
+            
+        # Afficher les transactions
+        else :
+            print("\n" + "=" * 40)
+            print(f"\n Historique des transactions pour le compte No : {self.__numero_compte} ")
+            print("=" * 40)
+            for transaction in self.historique :
+                print(f"\n Date : {transaction['date']}, \n Type : {transaction['type']}, \n Montant: {transaction['montant']} Fcfa, \n Solde apres transaction : {transaction['solde_apres_transaction']} Fcafa.")
+                
+        
 
 # Test de la classe CompteBancaire    
 test = CompteBancaire("Aziz", "F2345678")
@@ -60,17 +92,12 @@ print("-" * 50)
 # Effectuer un retrait
 test_retirer = test.retirer(2999.5677)
 print(test)
-print("-" * 50)
-
-# Afficher le solde
-test_solde = test.afficher_soldes()
-print(test)
-print("-" * 50)
 
 # infos du compte 
 test_infos = test.get_infos()
 print(test_infos)
-print("-" * 50)
+
+test_hostorique = test.afficher_historique()
 
 # Classe banque 
 # class Banque :
